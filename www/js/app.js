452a0879401d253a892218a2fc9fa0e09da32ce2
yutopc <yuto.tokunaga00@gmail.com>
2021-10-27 14:11:50 +0900

    a

--- www/js/app.js
+++ www/js/app.js
@@ -1,11 +1,11 @@
 
 
-$("#content1").click(function(){
+/*$("#content1").click(function(){
       $('.display_1').css('display','block'); 
       $('.display_2').css('display','none');
     });
 $("#content2").click(function(){
       $('.display_1').css('display','none'); 
       $('.display_2').css('display','block');
-    });
+    });*/
 /*ここまで検索画面のjs*/
