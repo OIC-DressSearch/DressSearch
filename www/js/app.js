048060ee65f6139a2e682f093cd2426645d83396
y-endo0085 <y.endo0085@gmail.com>
2021-11-02 13:23:18 +0900

    yu

--- www/js/app.js
+++ www/js/app.js
@@ -1,19 +1,52 @@
+/***共通部分*****************************************************/
 
 
-/*$("#content1").click(function(){
-      $('.display_1').css('display','block'); 
-      $('.display_2').css('display','none');
+  
+
+/***ここまで共通部分*****************************************************/
+
+/***検索画面*****************************************************/
+
+
+
+
+/***ここまで検索画面*****************************************************/
+
+/***一覧画面*****************************************************/
+
+
+
+
+/***ここまで一覧画面*****************************************************/
+
+/***いいね画面*****************************************************/
+
+  //いいね
+    $("body").on('click','.heart_enp',function(){
+      var element_id=$(this).attr('id');
+      $("#"+element_id).removeClass('heart_enp');
+      $("#"+element_id).addClass('heart');
     });
-$("#content2").click(function(){
-      $('.display_1').css('display','none'); 
-      $('.display_2').css('display','block');
-<<<<<<< HEAD
-    });*/
-/*$("#content3").click(function(){
-      $('.display_1').css('display','none'); 
-      $('.display_3').css('display','block');
-    });*/
-/*ここまで検索画面のjs*/
+
+  //いいね取消し
+    $("body").on('click','.heart',function(){
+      var element_id=$(this).attr('id');
+      $("#"+element_id).removeClass('heart');
+      $("#"+element_id).addClass('heart_enp');
+    });
+
+
+/***ここまでいいね画面*****************************************************/
+
+/***マイページ*****************************************************/
+
+  //遷移
+    
+
+
+/***ここまでマイページ*****************************************************/
+
+
 
 var apikey    = "7672355577f11839b1a72bce66af03d2a68e6f119b00178a4ecc8bc08daaaf68";
 var clientkey = "a35cc47c9dc52261c4590dae8f7d466eb3cdf83aa729c0443933ae8cb1d95b13";
@@ -35,16 +68,3 @@ $(".test").click(function(){
        });
 });
 
-//いいね
-$("body").on('click','.heart_enp',function(){
-  var element_id=$(this).attr('id');
-  $("#"+element_id).removeClass('heart_enp');
-  $("#"+element_id).addClass('heart');
-});
-
-//いいね取消し
-$("body").on('click','.heart',function(){
-  var element_id=$(this).attr('id');
-  $("#"+element_id).removeClass('heart');
-  $("#"+element_id).addClass('heart_enp');
-});
